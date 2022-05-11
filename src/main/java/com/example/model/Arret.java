d59f960cc5e3ec3dbd81f132306bebe09cbe4d78
l3miage-vabreb <97387167+l3miage-vabreb@users.noreply.github.com>
2022-05-11 13:42:07 +0200

    modification des models pour la generation des ids

--- src/main/java/com/example/model/Arret.java
+++ src/main/java/com/example/model/Arret.java
@@ -29,7 +29,7 @@ public class Arret {
     // // // // // // // //
     
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(
         name="identifiant",
         insertable = true,
